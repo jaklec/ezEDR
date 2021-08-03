c3af302b47f36728d4f6628d506c6e8403902387
Jakob Leczinsky <jakob.leczinsky@gmail.com>
2021-08-03 12:11:42 +0100

    Deployed a5cf684 with MkDocs version: 1.1.2

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  moduleFileExtensions: ["js", "json", "ts"],
-  testEnvironment: "node",
-  transform: {
-    "^.+\\.(t|j)s$": "ts-jest",
-  },
-  testRegex: "src/.+\\.test\\.(t|j)s",
-};
